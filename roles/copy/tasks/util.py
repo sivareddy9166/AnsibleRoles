8c62d084fe1a00633a2d2cf4ca122e7d5c17c756
Kondla Siva Kumar <92132610+sivareddy9166@users.noreply.github.com>
2023-03-12 18:13:50 +0530

    fix code

--- roles/copy/tasks/util.py
+++ roles/copy/tasks/util.py
@@ -1,8 +1,16 @@
 import psutil
 
 def get_cpu_temp():
-    temp = psutil.sensors_temperatures().get('coretemp')[0].current
-    return temp
+    sensors = psutil.sensors_temperatures()
+    if sensors.get('coretemp'):
+        temp = sensors['coretemp'][0].current
+        return temp
+    else:
+        return None
 
 if __name__ == '__main__':
-    print(f'CPU temperature: {get_cpu_temp()}C')
+    cpu_temp = get_cpu_temp()
+    if cpu_temp:
+        print(f'CPU temperature: {cpu_temp}C')
+    else:
+        print('Unable to retrieve CPU temperature.')
